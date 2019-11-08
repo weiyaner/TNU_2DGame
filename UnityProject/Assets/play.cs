c68cf33760075520ca767581cca6fc1622bbe8e7
weiyaner <57245498+weiyaner@users.noreply.github.com>
2019-11-08 12:05:50 +0800

    E40717040

---
+++ UnityProject/Assets/play.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class play : MonoBehaviour
+{
+    // 定義欄位 Field
+    // 修飾詞       欄位類型 欄位名稱 （指定 值）結尾
+    // 預設 private 私人 （在屬性面板上隱藏）
+    float speed = 3.5f;              // 浮點數  結尾要有 f
+    int jump = 300;                 // 整數
+    bool isGround = false;         //布林值 - true ` false
+    string name = "NHI";          // 字串 - 需要用雙引號
+}
